6dbfdd3904db1ab36ca1a42d8c8a02e0ce1a572b
emmanyouwell <emingala02@gmail.com>
2023-07-12 10:57:14 +0800

    Added Select2 for multiple Ingredients selection

--- resources/views/Admin/recipes/create.blade.php
+++ resources/views/Admin/recipes/create.blade.php
@@ -1,47 +1,203 @@
-@extends('Admin.index')
-@section('content')
-<div class="container">
-    <h2>Create Recipe</h2>
+ @extends('Admin.index')
 
-    <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" required>
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea class="form-control" id="description" name="description" required></textarea>
-        </div>
-        <div class="form-group">
-            <label for="instruction">Instruction</label>
-            <textarea class="form-control" id="instruction" name="instruction" required></textarea>
-        </div>
-        <div class="form-group">
-            <label for="category_id">Category</label>
-            <select class="form-control" id="category_id" name="category_id" required>
-                <option value="">Select Category</option>
-                @foreach ($categories as $id => $name)
-                    <option value="{{ $id }}">{{ $name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="ingredients">Select Ingredients:</label><br>
+@section('scriptsHead')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"
+integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg=="
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
+
+ @section('content')
+    <div class="container">
+        <h2>Create Recipe</h2>
+
+        <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="instruction">Instruction</label>
+                <textarea class="form-control" id="instruction" name="instruction" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="category_id">Category</label>
+                <select class="form-control" id="category_id" name="category_id" required>
+                    <option value="">Select Category</option>
+                    @foreach ($categories as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                {{-- <label for="ingredients">Select Ingredients:</label><br>
             @foreach ($ingredients as $ingredient)
                 <label>
                     <input type="checkbox" name="ingredients[]" value="{{ $ingredient->id }}">
                     {{ $ingredient->name }}
                 </label><br>
-            @endforeach
-        </div>
-    
-    
-        <div class="form-group">
-            <label for="image">Image</label>
-            <input type="file" class="form-control" id="image" name="image">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+            @endforeach --}}
+                <label for="ingredients">Select Ingredients:</label><br>
+                <select class="tags form-control" id="tags" name="tags[]" multiple="multiple"></select>
+                @error('tags')
+                    <label for="" class="text-danger">{{ $message }}</label>
+                @enderror
+            </div>
+
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" class="form-control" id="image" name="image">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
+ @section('scripts')
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        $(document).ready(function() {
+            $('.tags').select2({
+                placeholder: 'select',
+                allowClear: true,
+            });
+            $("#tags").select2({
+                ajax: {
+                    url: "{{ route('get-ingredient') }}",
+                    type: "post",
+                    delay: 250,
+                    dataType: 'json',
+                    data: function(params) {
+                        return {
+                            name: params.term,
+                            "_token": "{{ csrf_token() }}",
+                        };
+                    },
+                    processResults: function(data) {
+                        return {
+                            results: $.map(data, function(item) {
+                                return {
+                                    id: item.id,
+                                    text: item.name,
+                                }
+                            })
+                        }
+                    },
+                },
+            });
+        });
+    </script>
+@endsection 
+{{-- 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.2/css/bootstrap.min.css" integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"
+        integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Create Recipe</h2>
+
+        <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="instruction">Instruction</label>
+                <textarea class="form-control" id="instruction" name="instruction" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="category_id">Category</label>
+                <select class="form-control" id="category_id" name="category_id" required>
+                    <option value="">Select Category</option>
+                    @foreach ($categories as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-6 offset-md-3 form-group">
+                {{-- <label for="ingredients">Select Ingredients:</label><br>
+            @foreach ($ingredients as $ingredient)
+                <label>
+                    <input type="checkbox" name="ingredients[]" value="{{ $ingredient->id }}">
+                    {{ $ingredient->name }}
+                </label><br>
+            @endforeach 
+                <label for="ingredients">Select Ingredients:</label><br>
+                <select class="tags form-control" id="tags" name="tags[]" multiple="multiple"></select>
+                @error('tags')
+                    <label for="" class="text-danger">{{ $message }}</label>
+                @enderror
+            </div>
+
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" class="form-control" id="image" name="image">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+   
+    <script>
+        $(document).ready(function() {
+            $('.tags').select2({
+                placeholder: 'select',
+                allowClear: true,
+            });
+            $("#tags").select2({
+                ajax: {
+                    url: "{{ route('get-ingredient') }}",
+                    type: "post",
+                    delay: 250,
+                    dataType: 'json',
+                    data: function(params) {
+                        return {
+                            name: params.term,
+                            "_token": "{{ csrf_token() }}",
+                        };
+                    },
+                    processResults: function(data) {
+                        return {
+                            results: $.map(data, function(item) {
+                                return {
+                                    id: item.id,
+                                    text: item.name,
+                                }
+                            })
+                        }
+                    },
+                },
+            });
+        });
+    </script>
+   
+</body>
+
+</html> --}}
