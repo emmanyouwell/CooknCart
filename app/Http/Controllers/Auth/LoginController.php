3a52ff4342ba50976291f986fd2c55c3c883b45d
emmanyouwell <emingala02@gmail.com>
2023-07-13 14:37:30 +0800

    Fixed Admin & Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,11 +32,11 @@ class LoginController extends Controller
    // protected $redirectTo = RouteServiceProvider::HOME;
    protected function authenticated()
     {
-        if(Auth::user()->role_as == '1') //1 = Admin Login
+        if(Auth::user()->role_as == 1) //1 = Admin Login
         {
             return redirect('dashboard')->with('status','Welcome to your dashboard');
         }
-        elseif(Auth::user()->role_as == '0') // Normal or Default User Login
+        elseif(Auth::user()->role_as == 0) // Normal or Default User Login
         {
             return redirect('/home')->with('status','Logged in successfully');
         }
