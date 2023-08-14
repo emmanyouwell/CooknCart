18a173ef43abe5ece1f71694717c22da340750c2
tris-athena <lazaroaivy@gmail.com>
2023-08-14 14:27:43 +0800

    create instructions

--- resources/views/Admin/recipes/create.blade.php
+++ resources/views/Admin/recipes/create.blade.php
@@ -14,70 +14,87 @@
 
 
  @section('content')
- <div class="container">
-    <h2>Create Recipe</h2>
-    <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
-            @error('name')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
-            @error('description')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="instruction" class="form-label">Instruction</label>
-            <textarea class="form-control @error('instruction') is-invalid @enderror" id="instruction" name="instruction">{{ old('instruction') }}</textarea>
-            @error('instruction')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="category_id" class="form-label">Category</label>
-            <select class="form-control @error('category_id') is-invalid @enderror" id="category_id" name="category_id">
-                <option value="">Select Category</option>
-                @foreach ($categories as $id => $name)
-                    <option value="{{ $id }}" {{ old('category_id') == $id ? 'selected' : '' }}>{{ $name }}</option>
-                @endforeach
-            </select>
-            @error('category_id')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="tags" class="form-label">Select Ingredients:</label>
-            <select class="tags form-control @error('tags') is-invalid @enderror" id="tags" name="tags[]" multiple="multiple">
-                @foreach ($ingredients as $ingredient)
-                    <option value="{{ $ingredient->id }}" {{ in_array($ingredient->id, old('tags', [])) ? 'selected' : '' }}>{{ $ingredient->name }}</option>
-                @endforeach
-            </select>
-            @error('tags')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        {{-- <div class="mb-3">
+     <div class="container">
+         <h2>Create Recipe</h2>
+         <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
+             @csrf
+             <div class="mb-3">
+                 <label for="name" class="form-label">Name</label>
+                 <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                     name="name" value="{{ old('name') }}">
+                 @error('name')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             <div class="mb-3">
+                 <label for="description" class="form-label">Description</label>
+                 <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
+                 @error('description')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             <div class="mb-3" >
+                <table class="table table-bordered" id="instructions">
+                    <tr>
+                        <th>Instructions</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Enter instructions" class="form-control @error('instruction') is-invalid @enderror" id="instruction" name="instruction[]">{{ old('instruction') }}</input>
+                        </td>
+                        <td><button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary">Add steps</button></td>
+                    </tr>
+                </table>
+                 
+                 @error('instruction')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             <div class="mb-3">
+                 <label for="category_id" class="form-label">Category</label>
+                 <select class="form-control @error('category_id') is-invalid @enderror" id="category_id"
+                     name="category_id">
+                     <option value="">Select Category</option>
+                     @foreach ($categories as $id => $name)
+                         <option value="{{ $id }}" {{ old('category_id') == $id ? 'selected' : '' }}>
+                             {{ $name }}</option>
+                     @endforeach
+                 </select>
+                 @error('category_id')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             <div class="mb-3">
+                 <label for="tags" class="form-label">Select Ingredients:</label>
+                 <select class="tags form-control @error('tags') is-invalid @enderror" id="tags" name="tags[]"
+                     multiple="multiple">
+                     @foreach ($ingredients as $ingredient)
+                         <option value="{{ $ingredient->id }}"
+                             {{ in_array($ingredient->id, old('tags', [])) ? 'selected' : '' }}>{{ $ingredient->name }}
+                         </option>
+                     @endforeach
+                 </select>
+                 @error('tags')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             {{-- <div class="mb-3">
             <label for="images">Images:</label>
             <input type="file" name="images[]" id="images" multiple>
         </div> --}}
-        
-        
-        <div class="mb-3">
-            <label for="image" class="form-label">Image</label>
-            <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" name="image">
-            @error('image')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+
+
+             <div class="mb-3">
+                 <label for="image" class="form-label">Image</label>
+                 <input type="file" class="form-control @error('image') is-invalid @enderror" id="image"
+                     name="image">
+                 @error('image')
+                     <div class="invalid-feedback">{{ $message }}</div>
+                 @enderror
+             </div>
+             <button type="submit" class="btn btn-primary">Submit</button>
+         </form>
+     </div>
  @endsection
  @section('scriptFoot')
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -87,6 +104,18 @@
      <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"
          integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A=="
          crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+     <script type="text/javascript">
+         var i = 0;
+         $("#dynamic-ar").click(function() {
+             ++i;
+             $("#instructions").append('<tr><td><input type="text" name="instruction[' + i +
+            ']" placeholder="Enter instructions" class="form-control @error('instruction') is-invalid @enderror" /></td><td><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button></td></tr>'
+             );
+         });
+         $(document).on('click', '.remove-input-field', function() {
+             $(this).parents('tr').remove();
+         });
+     </script>
      <script>
          $(document).ready(function() {
              $('.tags').select2({
