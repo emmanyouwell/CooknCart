1bccb328a845ff1a53610924855c800ca69a4c87
emmanyouwell <emingala02@gmail.com>
2023-08-14 11:02:57 +0800

    fixed js scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -232,13 +232,14 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
     @yield('scriptFoot')
         {{-- Datatbles cdn's --}}
         {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
         {{-- <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet"> --}}
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
-        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+        
 </body>
 
 </html>
