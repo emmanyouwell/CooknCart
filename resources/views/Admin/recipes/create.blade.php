30eef3d645fee83358b3ce1ad040fbc1f8b12532
emmanyouwell <emingala02@gmail.com>
2023-08-16 16:13:22 +0800

    updated create form

--- resources/views/Admin/recipes/create.blade.php
+++ resources/views/Admin/recipes/create.blade.php
@@ -14,122 +14,140 @@
 
 
  @section('content')
-     <div class="container">
+     <div class="container card p-3 bg-light">
          <h2>Create Recipe</h2>
          <form action="{{ route('recipes.store') }}" method="POST" enctype="multipart/form-data">
              @csrf
-             <div class="mb-3">
-                 <label for="name" class="form-label">Name</label>
-                 <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                     name="name" value="{{ old('name') }}">
-                 @error('name')
-                     <div class="invalid-feedback">{{ $message }}</div>
-                 @enderror
+             <div class="row row-cols-1 row-cols-md-3 g-4">
+                 <div class="col-md-6 mb-4">
+                     <div class="mb-3">
+                         <label for="name" class="form-label">Name</label>
+                         <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                             name="name" value="{{ old('name') }}">
+                         @error('name')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                     <div class="mb-3">
+                         <label for="description" class="form-label">Description</label>
+                         <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                             rows="5">{{ old('description') }}</textarea>
+                         @error('description')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                 </div>
+                 <div class="col-md-6 mb-4">
+                     <div class="mb-3">
+                         <label for="preptime" class="form-label">Preparation Time</label>
+                         <input type="text" class="form-control @error('preptime') is-invalid @enderror" id="preptime"
+                             name="preptime">{{ old('preptime') }}</input>
+                         @error('preptime')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                     <div class="mb-3">
+                         <label for="cooktime" class="form-label">Cooking Time</label>
+                         <input type="name" class="form-control @error('cooktime') is-invalid @enderror" id="cooktime"
+                             name="cooktime">{{ old('cooktime') }}</input>
+                         @error('cooktime')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                     <div class="mb-3">
+                         <label for="servings" class="form-label">Servings</label>
+                         <input type="text" class="form-control @error('servings') is-invalid @enderror" id="servings"
+                             name="servings">{{ old('servings') }}</input>
+                         @error('servings')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                 </div>
              </div>
              <div class="mb-3">
-                 <label for="description" class="form-label">Description</label>
-                 <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
-                 @error('description')
-                     <div class="invalid-feedback">{{ $message }}</div>
-                 @enderror
-             </div>
-
-             <div class="mb-3">
-                <label for="preptime" class="form-label">Preparation Time</label>
-                <input type="text" class="form-control @error('preptime') is-invalid @enderror" id="preptime" name="preptime">{{ old('preptime') }}</input>
-                @error('preptime')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="cooktime" class="form-label">Cooking Time</label>
-                <input type="name" class="form-control @error('cooktime') is-invalid @enderror" id="cooktime" name="cooktime">{{ old('cooktime') }}</input>
-                @error('cooktime')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="servings" class="form-label">Servings</label>
-                <input type="text" class="form-control @error('servings') is-invalid @enderror" id="servings" name="servings">{{ old('servings') }}</input>
-                @error('servings')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-
-             <div class="mb-3">
-                 <table class="table table-bordered" >
+                 <table class="table table-bordered">
                      <tr>
                          <th>Instructions</th>
                          <th>Action</th>
                      </tr>
                      @php
-                         $i=0;
+                         $i = 0;
                      @endphp
                      <tbody id="instructions">
-                        <tr>
-                    
-                            <td><input type="text" placeholder="Enter instruction"
-                                    class="form-control @error('instruction') is-invalid @enderror" id="instruction"
-                                    name="instruction[{{$i}}][step]" value=""></input>
-                            </td>
-                            <td><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button></td>
-                            
-                            @php
-                            $i++;
-                            @endphp
-                        </tr>
-                    </tbody>
-                    <tr>
-                        <td colspan=2><button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary">Add
-                            steps</button></td>
-                    </tr>
+                         <tr>
+
+                             <td><input type="text" placeholder="Enter instruction"
+                                     class="form-control @error('instruction') is-invalid @enderror" id="instruction"
+                                     name="instruction[{{ $i }}][step]" value=""></input>
+                             </td>
+                             <td><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button>
+                             </td>
+
+                             @php
+                                 $i++;
+                             @endphp
+                         </tr>
+                     </tbody>
+                     <tr>
+                         <td colspan=2><button type="button" name="add" id="dynamic-ar"
+                                 class="btn btn-outline-primary">Add
+                                 steps</button></td>
+                     </tr>
                  </table>
 
                  @error('instruction')
                      <div class="invalid-feedback">{{ $message }}</div>
                  @enderror
              </div>
-             <div class="mb-3">
-                 <label for="category_id" class="form-label">Category</label>
-                 <select class="form-control @error('category_id') is-invalid @enderror" id="category_id"
-                     name="category_id">
-                     <option value="">Select Category</option>
-                     @foreach ($categories as $id => $name)
-                         <option value="{{ $id }}" {{ old('category_id') == $id ? 'selected' : '' }}>
-                             {{ $name }}</option>
-                     @endforeach
-                 </select>
-                 @error('category_id')
-                     <div class="invalid-feedback">{{ $message }}</div>
-                 @enderror
+             <div class="row row-cols-1 row-cols-md-3 g-4">
+                 <div class="col-md-6 mb-4">
+                     <div class="mb-3">
+                         <label for="category_id" class="form-label">Category</label>
+                         <select class="form-control @error('category_id') is-invalid @enderror" id="category_id"
+                             name="category_id">
+                             <option value="">Select Category</option>
+                             @foreach ($categories as $id => $name)
+                                 <option value="{{ $id }}" {{ old('category_id') == $id ? 'selected' : '' }}>
+                                     {{ $name }}</option>
+                             @endforeach
+                         </select>
+                         @error('category_id')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                 </div>
+                 <div class="col-md-6 mb-4">
+                     <div class="mb-3">
+                         <label for="tags" class="form-label">Select Ingredients:</label>
+                         <select class="tags form-control @error('tags') is-invalid @enderror" id="tags" name="tags[]"
+                             multiple="multiple">
+                             @foreach ($ingredients as $ingredient)
+                                 <option value="{{ $ingredient->id }}"
+                                     {{ in_array($ingredient->id, old('tags', [])) ? 'selected' : '' }}>
+                                     {{ $ingredient->name }}
+                                 </option>
+                             @endforeach
+                         </select>
+                         @error('tags')
+                             <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
+                     </div>
+                 </div>
              </div>
-             <div class="mb-3">
-                 <label for="tags" class="form-label">Select Ingredients:</label>
-                 <select class="tags form-control @error('tags') is-invalid @enderror" id="tags" name="tags[]"
-                     multiple="multiple">
-                     @foreach ($ingredients as $ingredient)
-                         <option value="{{ $ingredient->id }}"
-                             {{ in_array($ingredient->id, old('tags', [])) ? 'selected' : '' }}>{{ $ingredient->name }}
-                         </option>
-                     @endforeach
-                 </select>
-                 @error('tags')
-                     <div class="invalid-feedback">{{ $message }}</div>
-                 @enderror
-             </div>
-             {{-- <div class="mb-3">
-            <label for="images">Images:</label>
-            <input type="file" name="images[]" id="images" multiple>
-        </div> --}}
+             
 
 
-             <div class="mb-3">
+             <div class="mb-3" id="image-div">
                  <label for="image" class="form-label">Image</label>
                  <input type="file" class="form-control @error('image') is-invalid @enderror" id="image"
                      name="image">
                  @error('image')
                      <div class="invalid-feedback">{{ $message }}</div>
                  @enderror
+             </div>
+             <div class="mb-3">
+                 <a href="#" id="moreImage" class="text-primary">Add more images?</a>
+                 <a href="#" id="back" class="text-primary">Go back</a>
              </div>
              <button type="submit" class="btn btn-primary">Submit</button>
          </form>
@@ -145,16 +163,32 @@
          crossorigin="anonymous" referrerpolicy="no-referrer"></script>
      <script type="text/javascript">
          var i = 0;
+         $(document).ready(function(){
+            $('#back').hide();
+         });
          $("#dynamic-ar").click(function() {
-                    ++i;
-                    $("#instructions").append('<tr><td><input type="text" name="instruction[' + i +
-                        '][step]" placeholder="Enter instructions" class="form-control @error('instruction') is-invalid @enderror" /></td><td><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button></td></tr>'
-                    );
-                    
-                });
-        
+             ++i;
+             $("#instructions").append('<tr><td><input type="text" name="instruction[' + i +
+                 '][step]" placeholder="Enter instructions" class="form-control @error('instruction') is-invalid @enderror" /></td><td><button type="button" class="btn btn-outline-danger remove-input-field">Delete</button></td></tr>'
+             );
+
+         });
+
          $(document).on('click', '.remove-input-field', function() {
              $(this).parents('tr').remove();
+         });
+
+         $("#moreImage").click(function() {
+             $('#image-div').append('<div class="my-3 more"><label for="images">Add multiple images:</label><input type="file" class="form-control" name="images[]" id="images" multiple></div>');
+             $(this).hide();
+             $('#back').show();
+
+         });
+
+         $(document).on('click', '#back', function(){
+            $('.more').remove();
+            $(this).hide();
+            $('#moreImage').show();
          });
 
      </script>
